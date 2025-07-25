42611f27dabb692d7ba5e523b8a9e735470cd6a7
Cygarson <kacperlewicz122@gmail.com>
2025-07-25 23:00:39 +0200

    🎨 Release V2.9

--- server/server.js
+++ server/server.js
@@ -218,6 +218,7 @@ io.on("connection", (socket) => {
             }
 
             const summary = room.players.map(p => ({
+                id: p.id,
                 nickname: p.nickname,
                 color: p.color,
                 isImpostor: isImpostor(p),
@@ -273,6 +274,7 @@ io.on("connection", (socket) => {
         }
 
         const summary = room.players.map(pl => ({
+            id: pl.id,
             nickname: pl.nickname,
             color: pl.color,
             isImpostor: !pl.knowsWord && !pl.isKamikaze,
@@ -311,6 +313,7 @@ io.on("connection", (socket) => {
         if (!room || room.ownerId !== socket.id) return;
 
         const summary = room.players.map(p => ({
+            id: p.id,
             nickname: p.nickname,
             color: p.color,
             isImpostor: !p.knowsWord && !p.isKamikaze,
